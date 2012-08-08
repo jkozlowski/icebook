d0c5276ada6eddf8c00cfd40e64ee3c3602c34ea
Jakub Kozlowski <mail@jakub-kozlowski.com>
2012-08-08 21:15:08 +0100

    Add package-info.java to icebook.formatter package.

---
+++ src/main/java/icebook/formatter/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2012 Jakub Dominik Kozlowski <mail@jakub-kozlowski.com>
+ * Distributed under the The MIT License.
+ * (See accompanying file LICENSE)
+ */
+
+/**
+ * Defines the methods for appending to {@link Appendable} representations of {@link SortedSet}s of {@link Order}s.
+ *
+ * @author Jakub D Kozlowski
+ * @since 1.0
+ */
+package icebook.formatter;
+
+import java.util.SortedSet;
