7f081484ee71c95f5dea1759569e24d12cead987
Jakub Kozlowski <mail@jakub-kozlowski.com>
2012-08-08 10:41:58 +0100

    Add package-info.java for icebook.parser.

---
+++ src/main/java/icebook/parser/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2012 Jakub Dominik Kozlowski <mail@jakub-kozlowski.com>
+ * Distributed under the The MIT License.
+ * (See accompanying file LICENSE)
+ */
+
+/**
+ * Classes that define the grammar for parsing the input into new orders.
+ *
+ * @author Jakub D Kozlowski
+ * @since 1.0
+ */
+package icebook.parser;
