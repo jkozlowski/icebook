fb23c15c8000b8884619c011c2b508310c00f7ff
Jakub Kozlowski <mail@jakub-kozlowski.com>
2012-08-11 13:45:55 +0100

    Implement Trade.

--- src/main/java/icebook/exec/Trade.java
+++ src/main/java/icebook/exec/Trade.java
@@ -6,8 +6,82 @@
 
 package icebook.exec;
 
+import com.google.common.primitives.Longs;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.concurrent.Immutable;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
 /**
+ * Represents a trade.
+ *
  * @author Jakub D Kozlowski
+ * @since 1.0
  */
-public class Trade {
+@Immutable
+public final class Trade {
+
+    private final long buyOrderId;
+
+    private final long sellOrderId;
+
+    private final long price;
+
+    private final long quantity;
+
+    /**
+     * Default constructor.
+     *
+     * @param buyOrderId  id of the buy order.
+     * @param sellOrderId id of the sell order.
+     * @param price       execution price.
+     * @param quantity    quantity executed.
+     */
+    public Trade(@Nonnegative final long buyOrderId,
+                 @Nonnegative final long sellOrderId,
+                 @Nonnegative final long price,
+                 @Nonnegative final long quantity) {
+        checkArgument(Longs.min(buyOrderId, sellOrderId, price, quantity) > 0);
+        this.buyOrderId = buyOrderId;
+        this.sellOrderId = sellOrderId;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    /**
+     * Gets the buyOrderId.
+     *
+     * @return the buyOrderId.
+     */
+    public long getBuyOrderId() {
+        return buyOrderId;
+    }
+
+    /**
+     * Gets the sellOrderId.
+     *
+     * @return the sellOrderId.
+     */
+    public long getSellOrderId() {
+        return sellOrderId;
+    }
+
+    /**
+     * Gets the price.
+     *
+     * @return the price.
+     */
+    public long getPrice() {
+        return price;
+    }
+
+    /**
+     * Gets the quantity.
+     *
+     * @return the quantity.
+     */
+    public long getQuantity() {
+        return quantity;
+    }
 }
