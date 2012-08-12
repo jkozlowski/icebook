97a41e21b9da564802ee86c125c3b0de71749302
Jakub Kozlowski <mail@jakub-kozlowski.com>
2012-08-12 19:27:53 +0100

    Add package-info.java to icebook.book package.

---
+++ src/main/java/icebook/book/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2012 Jakub Dominik Kozlowski <mail@jakub-kozlowski.com>
+ * Distributed under the The MIT License.
+ * (See accompanying file LICENSE)
+ */
+
+/**
+ * Defines the interface for {@link OrderBook}s and {@link OrderBook.Entry}ies, and provides default implementations
+ * through {@link OrderBooks} factory.
+ *
+ * @author Jakub D Kozlowski
+ * @since 1.0
+ */
+package icebook.book;
+
