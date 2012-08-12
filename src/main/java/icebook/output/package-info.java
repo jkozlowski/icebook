3195be1a3258471322a4d6009a591eeb76a0dfc6
Jakub Kozlowski <mail@jakub-kozlowski.com>
2012-08-12 09:24:58 +0100

    Update package-info.java in icebook.output.

--- src/main/java/icebook/output/package-info.java
+++ src/main/java/icebook/output/package-info.java
@@ -5,11 +5,17 @@
  */
 
 /**
- * Defines the methods for appending to {@link Appendable} representations of {@link SortedSet}s of {@link Order}s.
+ * Defines the methods for appending to {@link Appendable} representations of {@link SortedSet}s of {@link Entry}ies,
+ * as well as {@link Collection}s of {@link Trade}s.
  *
  * @author Jakub D Kozlowski
  * @since 1.0
  */
 package icebook.output;
 
+import icebook.exec.Trade;
+
+import java.util.Collection;
 import java.util.SortedSet;
+
+import static icebook.book.OrderBook.Entry;
