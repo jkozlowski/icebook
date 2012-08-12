797ec8174c74fdb3b5aa1bf699b94ed9e814554d
Jakub Kozlowski <mail@jakub-kozlowski.com>
2012-08-12 19:32:48 +0100

    Add package-info.java to icebook.order package.

---
+++ src/main/java/icebook/order/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2012 Jakub Dominik Kozlowski <mail@jakub-kozlowski.com>
+ * Distributed under the The MIT License.
+ * (See accompanying file LICENSE)
+ */
+
+/**
+ * Defines the {@link Order} interface and exposes the iceberg and limit order implementation through {@link Orders}
+ * factory.
+ *
+ * @author Jakub D Kozlowski
+ * @since 1.0
+ */
+package icebook.order;
+
