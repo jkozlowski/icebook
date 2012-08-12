0df3c5f0e005196192e68b98d8fa7e12b77da178
Jakub Kozlowski <mail@jakub-kozlowski.com>
2012-08-12 19:29:38 +0100

    Add package-info.java to icebook.exec package.

---
+++ src/main/java/icebook/exec/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2012 Jakub Dominik Kozlowski <mail@jakub-kozlowski.com>
+ * Distributed under the The MIT License.
+ * (See accompanying file LICENSE)
+ */
+
+/**
+ * Defines the {@link ExecutionEngine} for manipulating and executing {@link Order}s on the {@link OrderBook}.
+ *
+ * @author Jakub D Kozlowski
+ * @since 1.0
+ */
+package icebook.exec;
+
+import icebook.book.OrderBook;
